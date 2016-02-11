9b3fd83e5666866acf151cfa392f38b2b9d002a3
Jan Vlcek <vlki@vlki.cz>
2016-02-11 20:38:02 +0100

    simulator works

--- first.py
+++ first.py
@@ -43,7 +43,7 @@ with open("busy_day.in") as f:
 def distance(origin, dest):
     return math.ceil(math.sqrt((origin[0] - dest[0]) ** 2 + (origin[1] - dest[1]) ** 2))
 
-print(distance((2, 1), (2, 2)))
+# print(distance((2, 1), (2, 2)))
 
 drones_status = []
 start_pos = warehouse_pos[0]
@@ -53,13 +53,49 @@ for di in range(drone_count):
     status = (start_pos, 0)
     drones_status.append(status)
 
-warehouse_status = warehouse_stock[:]
+warehouses_status = warehouse_stock[:]
 next_order = 0
-
-# for turn in range(turns):
-#     for drone_status in drones_status:
-#         if drone_status[1] == 0:
 #
+# def find_closest_warehouse_with_items(origin, items):
+#     for warehouse_index, warehouse_status in enumerate(warehouses_status):
+#         wh_has_items = true
+#         wh_distance = distance(warehouse_pos, warehouse_status)
+#         for items
+
+for turn in range(turns):
+    print("-- starting turn " + str(turn))
+    for drone_index, drone_status in enumerate(drones_status):
+        if drone_status[1] == 0:
+            drone_turn_start_pos = drone_status[0]
+            drone_turn_order_pos = order_pos[next_order]
+
+            # drone_turn_order_items = order_items[next_order]
+            # find_closest_warehouse_with_items(drone_turn_start_pos, drone_turn_order_items)
+
+            # TODO: find warehouse where to get stuff
+            drone_turn_distance = 0
+
+            # load stuff in warehouse
+            drone_turn_distance += 1
+
+            # travel to order pos
+            drone_turn_distance = distance(drone_turn_start_pos, drone_turn_order_pos)
+            print("sending drone " + str(drone_index) + " from " + str(drone_turn_start_pos[0]) + "-" + str(drone_turn_start_pos[1]) + " to " + str(drone_turn_order_pos[0]) + "-" + str(drone_turn_order_pos[1]) + " (distance " + str(drone_turn_distance) + ")")
+
+            # set the end position and when can we expect drone there
+            drones_status[drone_index] = (drone_turn_order_pos, drone_turn_distance)
+
+            next_order += 1
+
+            # do not plan next drone -> break
+            if next_order == order_count:
+                break
+
+        drones_status[drone_index] = (drones_status[drone_index][0], drones_status[drone_index][1] - 1)
+
+    # do not follow with any turns -> break
+    if next_order == order_count:
+        break
 
 
 
